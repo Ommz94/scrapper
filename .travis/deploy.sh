61475c81c79fa43f374cf094f39e4cc132ef3b99
Ommz94 <omeashgayan@gmail.com>
2020-02-12 10:15:58 +0530

    Update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,11 @@
 # print outputs and exit on first failure
 set -xe
 
+echo "Running add config.user"
 git config user.name "Ommz94"
+
+echo "Running add config.email"
 git config user.email "omeashgaya1@gmail.com"
 
+echo "Running pull"
 git pull origin master
