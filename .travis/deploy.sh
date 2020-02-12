e082efff0c86e7c0084edad2bd6a82765100131e
Ommz94 <omeashgayan@gmail.com>
2020-02-12 10:08:45 +0530

    Update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,12 +6,7 @@ set -xe
 # setup ssh agent, git config and remote
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/authorized_keys
-git remote add deploy "root@159.203.175.102:/var/www/html/scrapy"
 git config user.name "Ommz94"
 git config user.email "omeashgaya1@gmail.com"
 
-# commit compressed files and push it to remote
-git add .
-git status # debug
-git commit -m "Deploy compressed files"
-git push -f deploy HEAD:master
+git pull origin master
