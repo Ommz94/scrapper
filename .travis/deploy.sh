dc3cc4d2dd2f2531491e0d365f2d87cc231bfd75
Ommz94 <omeashgayan@gmail.com>
2020-02-12 10:12:31 +0530

    Update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,9 +3,6 @@
 # print outputs and exit on first failure
 set -xe
 
-# setup ssh agent, git config and remote
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/authorized_keys
 git config user.name "Ommz94"
 git config user.email "omeashgaya1@gmail.com"
 
